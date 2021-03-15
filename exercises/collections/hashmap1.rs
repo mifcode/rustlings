4f85c68ae271a9bbb82d6379a73d6c49d669e9ed
Maxwell I. French <maxwell@frenchnet.org>
2021-03-14 23:55:17 -0400

    Complete collections, strings, and quiz2.

--- exercises/collections/hashmap1.rs
+++ exercises/collections/hashmap1.rs
@@ -11,15 +11,20 @@
 // Execute the command `rustlings hint collections3` if you need
 // hints.
 
-// I AM NOT DONE
 
 use std::collections::HashMap;
 
 fn fruit_basket() -> HashMap<String, u32> {
-    let mut basket = // TODO: declare your hash map here.
+    let mut basket = HashMap::new(); // infers type based on usage and return value 
+    let mut test = HashMap::new(); // infers based on usage
 
     // Two bananas are already given for you :)
     basket.insert(String::from("banana"), 2);
+    basket.insert(String::from("tomato"), 3);
+    basket.insert(String::from("papaya"), 16);
+
+    test.insert(42, 69);
+    
 
     // TODO: Put more fruits in your basket here.
 
